8ac34b3088c9451d317949e1d24851b62a97204c
Patrick Ramirez <14876646+rpramirez87@users.noreply.github.com>
2025-02-12 13:49:58 -0500

    Add main app for Pro SwiftUI

---
+++ Pro SwiftUI/Pro SwiftUI/Pro_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pro_SwiftUIApp.swift
+//  Pro SwiftUI
+//
+//  Created by Patrick Ramirez on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Pro_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
